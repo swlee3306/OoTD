f360165ffab9494250066f49523f4d0584c6db90
swlee <swlee3306@gmail.com>
2022-01-11 22:13:10 +0900

    Initial Commit

---
+++ OoTD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OoTD
+//
+//  Created by sanguk lee on 2022/01/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OoTD/OoTDApp.swift
@@ -0,0 +1,17 @@
+//
+//  OoTDApp.swift
+//  OoTD
+//
+//  Created by sanguk lee on 2022/01/11.
+//
+
+import SwiftUI
+
+@main
+struct OoTDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
