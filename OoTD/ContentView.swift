33ba4b4eea50b26d7bd69cfc0577926b2a1dc264
swlee <swlee3306@gmail.com>
2022-01-25 23:05:59 +0900

    01/25/21 -> Title 설정 및 네비게이션 바 오류 수정

--- OoTD/ContentView.swift
+++ OoTD/ContentView.swift
@@ -10,80 +10,88 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        ZStack{
+        NavigationView {
             
-            Image("Background_Image")
+            ZStack{
+                
+                Image("Background_Image")
+                
+                
+                VStack(alignment: .trailing, spacing: 0){
+                    
+                    
+                    Divider()
+                    
+                    
+                    NavigationLink(destination: List_View()){
+                        HStack{
+                            
+                            Image(systemName: "list.star")
+                                .font(.system(size: 40))
+                                .foregroundColor(.white)
+                                .padding(.leading, 30)
+                            
+                            Text("List")
+                                .font(.system(size: 30))
+                                .bold()
+                                .foregroundColor(.white)
+                                .padding([.top, .bottom, .trailing], 25)
+                            
+                        }.background(Color.black)
+                            .cornerRadius(40)
+                            .padding(.bottom)
+                    }
+                    
+                    
+                    NavigationLink(destination: Preference_View()){
+                        HStack{
+                            
+                            Image(systemName: "sparkles")
+                                .font(.system(size: 40))
+                                .foregroundColor(.white)
+                                .padding(.leading, 30)
+                            
+                            Text("Preference")
+                                .font(.title)
+                                .bold()
+                                .foregroundColor(.white)
+                                .padding([.top, .bottom, .trailing], 25)
+                            
+                        }.background(Color.black)
+                            .cornerRadius(40)
+                            .padding(.bottom)
+                    }
+                    
+                    
+                    NavigationLink(destination: Calendar_View()){
+                        HStack{
+                            
+                            Image(systemName: "calendar")
+                                .font(.system(size: 40))
+                                .foregroundColor(.white)
+                                .padding(.leading, 30)
+                            
+                            Text("Calendar")
+                                .font(.system(size: 30))
+                                .bold()
+                                .foregroundColor(.white)
+                                .padding([.top, .bottom, .trailing], 25)
+                            
+                        }.background(Color.black)
+                            .cornerRadius(40)
+                            .padding(.bottom)
+                    }
+                    
+                }.padding(.top, 300)
+                
+            }.edgesIgnoringSafeArea(.all)
             
-            VStack(alignment: .trailing, spacing: 0){
-                
-                Divider()
-                
-                
-                NavigationLink(destination: List_View()){
-                    HStack{
-                        
-                        Image(systemName: "list.star")
-                            .font(.system(size: 40))
-                            .foregroundColor(.white)
-                            .padding(.leading, 30)
-                        
-                        Text("LIST")
-                            .font(.system(size: 30))
-                            .bold()
-                            .foregroundColor(.white)
-                            .padding([.top, .bottom, .trailing], 25)
-                        
-                    }.background(Color.black)
-                        .cornerRadius(40)
-                        .padding(.bottom)
-                }
-                
-                NavigationLink(destination: Preference_View()){
-                    HStack{
-                        
-                        Image(systemName: "sparkles")
-                            .font(.system(size: 40))
-                            .foregroundColor(.white)
-                            .padding(.leading, 30)
-                        
-                        Text("Preference")
-                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                            .bold()
-                            .foregroundColor(.white)
-                            .padding([.top, .bottom, .trailing], 25)
-                        
-                    }.background(Color.black)
-                        .cornerRadius(40)
-                        .padding(.bottom)
-                }
-                
-                
-                
-                NavigationLink(destination: Calendar_View()){
-                    HStack{
-                        
-                        Image(systemName: "calendar")
-                            .font(.system(size: 40))
-                            .foregroundColor(.white)
-                            .padding(.leading, 30)
-                        
-                        Text("Calendar")
-                            .font(.system(size: 30))
-                            .bold()
-                            .foregroundColor(.white)
-                            .padding([.top, .bottom, .trailing], 25)
-                        
-                    }.background(Color.black)
-                        .cornerRadius(40)
-                        .padding(.bottom)
-                }
-            }.padding(.top, 300)
+                .navigationBarItems(leading: Text("OoTD").font(.system(size : 50).bold()).foregroundColor(.white).padding(10).background(Color.black).cornerRadius(10).padding(.top, 20))
             
-        }.edgesIgnoringSafeArea(.all)
-        
+            
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
